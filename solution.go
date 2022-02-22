4e2a3e58bbe0a72b3794d4e2d992e0f6a1ec4a56
Ivan Victorov <victorov.i.vl@yandex.ru>
2022-02-22 03:03:22 +0300

    Init project

--- solution.go
+++ solution.go
@@ -1,7 +1,38 @@
 package solution
 
-import "github.com/kyokomi/emoji/v2"
+import (
+	"math"
+)
 
-func GetMessage() string {
-	return emoji.Sprintf("Hello :world_map:!")
+type intCustomType int
+
+func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
+	var result float64
+
+	switch sidesNum {
+	case intCustomType(0):
+		result = calcAreaCircle(sideLen)
+	case intCustomType(3):
+		result = calcAreaTriangle(sideLen)
+	case intCustomType(4):
+		result = calcAreaSquare(sideLen)
+	default:
+		result = 0
+	}
+
+	return result
+}
+
+func calcAreaTriangle(sideLen float64) float64 {
+	var halfPerimetr float64
+	halfPerimetr = (3 * sideLen) / 2
+	return math.Sqrt(halfPerimetr * (halfPerimetr - sideLen) * (halfPerimetr - sideLen) * (halfPerimetr - sideLen))
+}
+
+func calcAreaSquare(sideLen float64) float64 {
+	return sideLen * sideLen
+}
+
+func calcAreaCircle(sideLen float64) float64 {
+	return math.Pi * sideLen * sideLen
 }
